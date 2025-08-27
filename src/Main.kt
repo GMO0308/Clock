8a01e868504038a08d0731b64994d7d1e2973832
GMO0308 <tim,anesi08@gmail.com>
2025-08-27 10:07:41 +0800

    finale

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Activity 2: TestClock.kt
+// Program to test the Clock class
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    val clock = Clock()
+    var input = ' '
+    do {
+        // Display current time
+        println("The current time ==>${clock.day}:${clock.hour}:${clock.minute}:${clock.second}")
+        println("Enter 's' to set the time.")
+        println("Enter 'a' to advance one second.")
+        println("Enter 'r' to reset the clock.")
+        println("Enter 'q' to quit.")
+        input = readlnOrNull()?.firstOrNull() ?: continue
+        when (input) {
+            's' -> {
+                // Set time from user input
+                print("Enter hour (0-23): ")
+                clock.hour = readlnOrNull()?.toIntOrNull() ?: 0
+                print("Enter minute (0-59): ")
+                clock.minute = readlnOrNull()?.toIntOrNull() ?: 0
+                print("Enter second (0-59): ")
+                clock.second = readlnOrNull()?.toIntOrNull() ?: 0
+            }
+            'a' -> clock.advanceOneSecond() // Advance time by one second
+            'r' -> clock.reset() // Reset clock to 00:00:00
+        }
+    } while (input != 'q') // Exit on 'q'
 }
